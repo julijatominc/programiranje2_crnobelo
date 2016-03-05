2b101a508e7ef1f89ae45d16b5c5c7ca2ec95dac
SeverinMejak <severin.mejak@hotmail.com>
2016-03-05 16:51:09 +0100

    Dodaj funkcije v razred tabla in funkcije v GUI

--- crnobelo.py
+++ crnobelo.py
@@ -1,4 +1,5 @@
 from tkinter import*
+import ast
 
 
 ######################################################################
@@ -13,7 +14,15 @@ VELIKOST = 5
 ## Igra
 
 class tabla():
-    pass
+    def __init__(self):
+        self.matrika = [[(True, True)] * self.velikost for _ in range(self.velikost)]
+        self.na_vrsti = JAZ
+
+        def dovoljeno(self, x, y):
+            pass
+
+        def konec_igre(self):
+            pass
 
 ######################################################################
 ## Uporabniski vmesnik
@@ -84,24 +93,97 @@ class crnobelo():
         else:
             self.canvas.delete(crnobelo.TAG_KROG)
 
-        self.matrika1 = [[PRAZNO] * self.velikost for _ in range(self.velikost)]
-        self.matrika2 = [[PRAZNO] * self.velikost for _ in range(self.velikost)]
+        self.matrika1 = [[(True, True)] * self.velikost for _ in range(self.velikost)]
         self.napis.set("")
         self.na_vrsti = JAZ
 
     # Funkcija, ki reagira na izbiro polja s strani uporabnika.
     def izberi(self, event):
-        pass
+        self.napis.set("")
+        x, y = (event.x - 50) // 100, (event.y - 50) // 100
+        # Preveri, ce je konec igre. V primeru, da je konec, nocemo vec dogajanja na plosci.
+        if not self.konec_igre():
+            # Preveri, ce je poteza dovoljena.
+            if self.dovoljeno(x, y):
+                if self.na_vrsti == JAZ:
+                    # Spremeni matriko, v kateri imamo zapisano katere poteze so mozne.
+                    self.spremeni_matriko(x, y, 1)
+                    # Na potezi je nasprotnik.
+                    self.na_vrsti = ON
+                    # Narise krog.
+                    self.canvas.create_oval(x * 100 + 60, y * 100 + 60, x * 100 + 140, y * 100 + 140)
+                else:
+                    self.spremeni_matriko(x, y, 0)
+                    self.na_vrsti = JAZ
+                    self.canvas.create_oval(x * 100 + 60, y * 100 + 60, x * 100 + 140, y * 100 + 140, fill="black")
+
+                if self.konec_igre():
+                    if self.na_vrsti == JAZ:
+                        self.na_vrsti = ON
+                    else:
+                        self.na_vrsti = JAZ
+                    self.napis.set("Konec igre! Zmagal je {0}!".format(self.na_vrsti))
+
+            else:
+                self.napis.set("Neveljavna poteza!")
 
     # Funkcija, ki shrani igro v datoteko.
     def shrani(self):
-        pass
+        ime = filedialog.asksaveasfilename()
+        if ime == "":
+            return
+        with open(ime, "wt", encoding="utf8") as f:
+            print(self.matrika1, file=f)
+            print(self.na_vrsti, file=f)
+            print(self.velikost, file=f)
 
     # Funkcija, ki nalozi igro iz datoteke.
     def odpri(self):
-        pass
+        ime = filedialog.askopenfilename()
+        s = open(ime, encoding="utf8")
+        sez = s.readlines()
+        s.close
 
+        self.matrika = ast.literal_eval(sez[0].strip())
+        KDO = sez[1].strip()
+        velikost = sez[2].strip()
 
+        self.nova_igra(velikost)
+
+        if KDO == str("Beli"):
+            self.na_vrsti = JAZ
+        else: self.na_vrsti = ON
+
+        for i in range(self.velikost):
+            for j in range(self.velikost):
+                if self.matrika[i][j][0]:
+                    self.canvas.create_oval(i * 100 + 60, j * 100 + 60, i * 100 + 140, j * 100 + 140)
+                if self.matrika[i][j][1]:
+                    self.canvas.create_oval(i * 100 + 60, j * 100 + 60, i * 100 + 140, j* 100 + 140, fill="black")
+
+    # Funkcija, ki potezo zapise v matriko in hkrati doloci katere poteze so mogoce.
+    def spremeni_matriko(self, x, y, n):
+        self.matrika[x][y][0] = False
+        self.matrika[x][y][1] = False
+        try:
+            self.matrika[x-1][y][n] = False
+        except:
+            pass
+
+        try:
+            self.matrika[x+1][y][n] = False
+        except:
+            pass
+
+        try:
+            self.matrika[x][y-1][n] = False
+        except:
+            pass
+
+        try:
+            self.matrika[x][y+1][n] = False
+        except:
+            pass
 
 ######################################################################
 ## Igralec minimax
